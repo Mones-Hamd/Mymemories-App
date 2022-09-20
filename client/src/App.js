25a08753d9b6fb57c32434d4ebc1d807744bdb21
Mones Hamd <mones.m.azzam91@gmail.com>
2022-09-20 20:23:20 +0200

    initial context for post and build form to create posts

--- client/src/App.js
+++ client/src/App.js
@@ -2,42 +2,44 @@ import React from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import memories from './imgs/memories.jpeg';
 import Posts from './components/Posts/Posts';
-
 import Form from './components/Form/Form';
 import useStyles from './Styles';
+import { PostProvider } from './context/PostsContect';
 const App = () => {
   const classes = useStyles();
   return (
-    <Container maxwidth="lg">
-      <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">
-          MY Memories{' '}
-        </Typography>
-        <img
-          className={classes.image}
-          src={memories}
-          alt="memories"
-          height="60"
-        />
-      </AppBar>
-      <Grow in>
-        <Container>
-          <Grid
-            container
-            justify="space-between"
-            alignItems="stretch"
-            spacing={3}
-          >
-            <Grid xs={12} sm={7}>
-              <Posts />
+    <PostProvider>
+      <Container maxwidth="lg">
+        <AppBar className={classes.appBar} position="static" color="inherit">
+          <Typography className={classes.heading} variant="h2" align="center">
+            MY Memories{' '}
+          </Typography>
+          <img
+            className={classes.image}
+            src={memories}
+            alt="memories"
+            height="60"
+          />
+        </AppBar>
+        <Grow in>
+          <Container>
+            <Grid
+              container
+              justify="space-between"
+              alignItems="stretch"
+              spacing={3}
+            >
+              <Grid xs={12} sm={7}>
+                <Posts />
+              </Grid>
+              <Grid xs={12} sm={4}>
+                <Form />
+              </Grid>
             </Grid>
-            <Grid xs={12} sm={4}>
-              <Form />
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
-    </Container>
+          </Container>
+        </Grow>
+      </Container>
+    </PostProvider>
   );
 };
 
